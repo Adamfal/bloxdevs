d4f2d00b186f3556548fd7b70f5c7d55bb389892
Adamfal <103952300+Adamfal@users.noreply.github.com>
2022-10-02 02:52:22 +0200

    Delete members.js

--- src/Commands/members.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    name: "members",
-    description: "BloxDevs Members!",
-    run: async (client,interaction) => {
-        return interaction.followUp({ content:`**Members**: ${interaction.guild.memberCount}` });
-    }
-}
